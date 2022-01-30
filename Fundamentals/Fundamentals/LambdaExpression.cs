2803561f33ebbf35db1ddc90201cd0af3f12dd72
subramanian <subramanian.nk@live.com>
2022-01-30 01:18:32 -0600

    Lambda expression done

--- Fundamentals/Fundamentals/LambdaExpression.cs
+++ Fundamentals/Fundamentals/LambdaExpression.cs
@@ -42,12 +42,62 @@ namespace Fundamentals
             double minCatName = catNames.Min(cat => cat.Length);
 
             Console.WriteLine($"The average cat name is {avgCatName}, maximum is {maxCatName}, minimum is {minCatName} ");
-
-
+            
             var allIntegers = mix.OfType<int>();
             Console.WriteLine(string.Join(",", allIntegers));
 
+            var allIntegersList = mix.OfType<int>().ToList();
+            foreach(var x in allIntegersList)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+            var allIntegersLessthan4 = mix.OfType<int>().Where(n => (n < 4));
+            Console.WriteLine("less than 4 " + string.Join(",", allIntegersLessthan4));
+
+
+
+
+            var integerListavailable = mix.OfType<List<int>>();
+            var ListofintegerList = integerListavailable.ToList();
+
+            int temp = 1;
+            foreach(var y in ListofintegerList)
+            {
+                
+                Console.WriteLine($"List {temp++} is "+string.Join(",", y));
+            }
+
+            var warriors = new List<Warrior>();
+            warriors.Add(new Warrior("zfa", 134));
+            warriors.Add(new Warrior("zda", 234));
+            warriors.Add(new Warrior("zad", 133));
+            warriors.Add(new Warrior("qza", 164));
+            warriors.Add(new Warrior("ab", 334));
+            warriors.Add(new Warrior("aa", 346));
+            warriors.Add(new Warrior("aw", 343));
+            warriors.Add(new Warrior("ae", 334));
+            warriors.Add(new Warrior("al", 343));
+
+
+            var warriorheight = warriors.Select(s => s.height);
+            Console.WriteLine("warrior heights are below");
+            Console.WriteLine(string.Join("\n", warriorheight));
+
+            var shortwarriors = warriors.Where(x => (x.height < 300)).Select(x => x.height);
+            Console.WriteLine(string.Join("\n", shortwarriors));
+
+            string[] namesLast = { "subbu", "hjkh", "gejkfgku", "dviuewg", "ejf" };
+            //namesLast.ForEach(x=>Console.WriteLine(x));
+            warriors.ForEach(x => Console.WriteLine(x.height));
+
+
+
         }
+
+
 
 
     }
---
+++ Fundamentals/Fundamentals/Warrior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    public class Warrior
+    {
+        public string name;
+        public int height;
+
+        public Warrior(string name, int height)
+        {
+            this.name = name;
+            this.height = height;
+        }
+           
+
+    }
+}
